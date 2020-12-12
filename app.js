5928cc0b946c74c3ccd9b3403546bc8655d0fbff
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-12 22:33:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers':'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+        }; 
+
+app.use((r, res, next) => { r.res.set(CORS); next(); });
+
 app.get('/', (req, res) => res.send('andreipavlevich'));
 
 app.listen(process.env.PORT);
