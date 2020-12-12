5e33b214b54851702017e4413c4b7ee990b0f022
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-12 23:41:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
     
+
+app.get('/login/' (req, res) => res.send('login:andrei'));
+
 app.all('/*', (req, res) => 
 {
     res.send('andreipavlevich');
