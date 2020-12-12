3f875bd8fc4877e5a57809599d7d1f1fa97b2982
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-12 23:52:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.get('/login/', (req, res) => res.send('login:andrei'));
 
+app.get('/test/', async (req,res) =>
+    {
+      await res.set('Content-Type', 'application/json');
+      const URL = 'https://kodaktor.ru/g/80b5cdf';
+      const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+      const page = await browser.newPage();
+      await page.goto(URL);
+      await page.click('#bt');
+      await page.waitForSelector('#inp');
+      res.send(page.querySelector('#inp').value);
+});
+
 app.all('/*', (req, res) => res.send('andreipavlevich'));
 
 app.listen(process.env.PORT)
