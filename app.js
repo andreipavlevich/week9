5a4bd71fa7ec31b8dc34c54015bb142062c8640a
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-12 22:51:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
+//const bodyParser = require('body-parser');
+//const puppeteer = require('puppeteer');
 
 const app = express();
 
