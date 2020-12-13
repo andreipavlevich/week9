c8dfe16b6ef48afe152c416cefcaa6da8e5b1ae5
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-13 12:07:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ app.get('/login/', (req, res) => res.send('login:andrei'));
 app.get('/test/', async (req,res) =>
     {
       const URL = 'https://kodaktor.ru/g/bb4613b';
-      const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+      const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
       const page = await browser.newPage();
       await page.goto(URL);
-      await page.waitForSelector('#inp')
-      const x = 'hello';
-      page.evaluate(x => document.querySelector('#inp').value = x, x);
-      await page.waitForSelector('#bt');
-      await page.click('#bt');
-      const got = await page.$eval('#inp', el => el.value);
-      console.log(got);
+      //await page.waitForSelector('#inp')
+      //const x = 'hello';
+      //page.evaluate(x => document.querySelector('#inp').value = x, x);
+      //await page.waitForSelector('#bt');
+      //await page.click('#bt');
+      //const got = await page.$eval('#inp', el => el.value);
+      //console.log(got);
       await browser.close();
 });
 
