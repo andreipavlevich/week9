4ce7636478d2a9e6fc04d1439c932568a6cd75f1
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-13 14:21:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ app.get('/test/', async (req,res,next) =>
 {
       
           
-const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-const page = await browser.newPage();
-await page.goto('http://example.com');
-await page.screenshot({path: 'example.png'});
-res.send('screen grabbed');
-browser.close();
+// const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+// const page = await browser.newPage();
+// await page.goto('http://example.com');
+// await page.screenshot({path: 'example.png'});
+// res.send('screen grabbed');
+// browser.close();
           
-     // const URL = 'https://kodaktor.ru/g/bb4613b';
-     // const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-     // const page = await browser.newPage();
-     // await page.goto(URL);
-    //  await page.waitForSelector('#inp');
-    //  await page.waitForSelector('#bt');
-    //  await page.click('#bt');
-    //  const got = await page.$eval('#inp', el => el.value);
+      const URL = 'https://kodaktor.ru/g/bb4613b';
+      const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      const page = await browser.newPage();
+      await page.goto(URL);
+      await page.waitForSelector('#inp');
+      await page.waitForSelector('#bt');
+      await page.click('#bt');
+      const got = await page.$eval('#inp', el => el.value);
     //  console.log(got);
-    //  await browser.close();
+      res.send('hash is ok');
+      await browser.close();
 
 
 });
