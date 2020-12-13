8d40b6bee53979738c19d2268ea86404c82b833e
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-13 13:17:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
+    
+app.get('/login/', (req, res) => res.send('login:andrei'));
 
-async function getHASH() {
+app.get('/test/', (req,res,next) => 
+{
+      (async () => {
       const URL = 'https://kodaktor.ru/g/bb4613b';
       const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
       const page = await browser.newPage();
+            try {
       await page.goto(URL);
-      await page.waitForSelector('#inp')
-      const x = 'hello';
-      page.evaluate(x => document.querySelector('#inp').value = x, x);
-      await page.waitForSelector('#bt');
-      await page.click('#bt');
-      const got = await page.$eval('#inp', el => el.value);
-      console.log(got);
-      await browser.close();
-      return got;
-  }
-    
-
-app.get('/login/', (req, res) => res.send('login:andrei'));
-
-app.get('/test/', (req,res) => res.send(getHASH()));
+            }
+            catch (err)
+            {
+                  res.send('No URL');
+                  
+            }
+    //  await page.waitForSelector('#inp');
+    //  await page.waitForSelector('#bt');
+    //  await page.click('#bt');
+    //  const got = await page.$eval('#inp', el => el.value);
+    //  console.log(got);
+    //  await browser.close();
+  })();
+});
 
 app.all('/*', (req, res) => res.send('andreipavlevich'));
 
