80233c97c54d96e1b74c684d9ca20f7ebb9a8678
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2020-12-13 14:42:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 const app = express();
+
+const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers':'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+        }; 
+    
+
+app.use((r, res, next) => { r.res.set(CORS); next(); });
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
     
 app.get('/login/', (req, res) => res.send('login:andrei'));
 
